580702313f0e4ca4842c04855cc0c5b26bccdae3
jcl <jcl@JCL-I7>
2020-04-21 12:00:53 +0200

    adding searching feature to glossary

--- src/Glossary.java
+++ src/Glossary.java
@@ -1,27 +1,23 @@
 import java.util.ArrayList;
 import java.util.Collections;
 
-
-
-
 /**
  * @author jcl
  *
  */
 public class Glossary {
 
-
 	private ArrayList<Word> glossary = new ArrayList<Word>();
+
 	/**
 	 * @param glossary
 	 */
 	public Glossary() {
-	}	
-	
-	public void sort(){ //sort words alphabetically
-		Collections.sort(glossary);
 	}
 
+	public void sort() { // sort words alphabetically
+		Collections.sort(glossary);
+	}
 
 	/**
 	 * @return the glossary
@@ -30,19 +26,28 @@ public class Glossary {
 		return glossary;
 	}
 
-
 	/**
 	 * @param glossary the glossary to set
 	 */
 	public void setGlossary(ArrayList<Word> glossary) {
 		this.glossary = glossary;
 	}
-	
+
 	public void addWordGlossary(Word word) {
 		glossary.add(word);
 	}
-	
-	public void modifier() {
-		
+
+	public Word search(String keyWord) {
+
+		for (Word w : this.glossary) {
+			if (w.getEnglishWord().contains("keyWord")) {
+				return w;
+			}
+		}
+		return null;
+	}
+
+	public void modify() {
+
 	}
 }
