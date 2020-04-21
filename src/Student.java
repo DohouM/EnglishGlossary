c2cbffe8c827cce9c2c7820dfacc16bf972c80cf
jcl <jcl@JCL-I7>
2020-04-21 09:48:09 +0200

    fixing problems about students

--- src/Student.java
+++ src/Student.java
@@ -7,5 +7,24 @@
  *
  */
 public class Student extends User {
+	public Student(String userName) {
+		super(userName);
+		// TODO Auto-generated constructor stub
+	}
+
 	private String group;
+
+	/**
+	 * @return the group
+	 */
+	public String getGroup() {
+		return group;
+	}
+
+	/**
+	 * @param group the group to set
+	 */
+	public void setGroup(String group) {
+		this.group = group;
+	}
 }
