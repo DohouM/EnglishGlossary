a777519fb4f33a7092fa0fcae64d1992b01056bf
DOHOU Medhy <medhy.fournier3@gmail.com>
2020-05-11 13:08:01 +0200

    Changes to Glossary.java : + Added javadoc. - Deprecated setGlossary method. ~ Changed constructor to take an ArrayList of Word objects as a parameter, then initialize the glossary as the parameter passed. ~ Turned numberOfWordsPerPage to a non-static variable. (Specific to object)

--- src/Glossary.java
+++ src/Glossary.java
@@ -2,66 +2,96 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 /**
- * @author jcl
- *
+ * 
+ * Glossary is a class meant to contain an English-French technical glossary.
+ * @author Jean-Camille LAPIERRE, Dorian MOUNIER, Gabriel SAPONARA, Cyril
+ *         PIGEYRE, Medhy DOHOU
+ * @version 1.4
+ * 
+ * 
  */
 
 public class Glossary {
 
-	private final static int numberOfWordPerPage = 51;
+    private final int numberOfWordPerPage = 51;
+    
+    /**
+     * The glossary stored into the object.
+     */
+    private ArrayList<Word> glossary;
 
-	private ArrayList<Word> glossary;
+    /**
+     * Create a new Glossary object, with the <tt>glossary</tt>
+     * @param glossary : The glossary to contain.
+     */
+    public Glossary(ArrayList<Word> glossary) {
+	this.glossary = glossary;
+    }
+    
+    /**
+     * Sort glossary by lexical order using english words.
+     */
+    public void sortByEn() {
+	Collections.sort(glossary);
+    }
 
-	/**
-	 * @param glossary
-	 */
-	public Glossary() {
-		this.glossary = new ArrayList<Word>();
+    /**
+     * Glossary getter.
+     * @return An array list with all the words of the glossary.
+     */
+    public ArrayList<Word> getGlossary() {
+	return glossary;
+    }
+
+    /**
+     * @deprecated
+     * Take a glossary as a parameter.
+     * @param glossary : The glossary to contain.
+     */
+    public void setGlossary(ArrayList<Word> glossary) {
+	this.glossary = glossary;
+    }
+    
+    /** 
+     * Add a word to the glossary, and sort it again using the method <tt>sortByEn</tt>
+     * @param word : The word to add into the glossary.
+     */
+
+    public void addWordGlossary(Word word) {
+	this.glossary.add(word);
+	this.sortByEn();
+    }
+
+    /**
+     * Same method as <tt>addWordGlossary</tt>, except it takes strings as an input rather than a <tt>Word</tt> object.
+     * @param EnglishWord :  The English word to add (must be a <tt>String</tt>).
+     * @param FrenchWord :  The french translation of the word (must be a <tt>String</tt>).
+     */
+    public void addWordStringOnly(String englishWord, String frenchWord) {
+	Word word = new Word(englishWord, frenchWord);
+	glossary.add(word);
+	this.sortByEn();
+    }
+
+    /**
+     * Search through the glossary for the string passed as a parameter, and return all the results.
+     * @param keyWord : The word to search (can be either french or english).
+     * @return The word that matches, else returns <tt>keyWord not found</tt>.
+     */
+    public String search(String keyWord) {
+//ArrayList ï¿½ faire pour le return ->ajouter les mots ->plusieurs mots mï¿½me traduction
+	for (Word w : this.glossary) {
+	    if (w.getEnglishWord().contains(keyWord)) {
+		return w.toString();
+	    }
 	}
-
-	public void sortByEn() { // sort words alphabetically
-		Collections.sort(glossary);
+	for (Word w : this.glossary) {
+	    if (w.getFrenchWord().contains(keyWord)) {
+		return w.toString();
+	    }
 	}
-
-	/**
-	 * @return the glossary
-	 */
-	public ArrayList<Word> getGlossary() {
-		return glossary;
-	}
-
-	/**
-	 * @param glossary the glossary to set
-	 */
-	public void setGlossary(ArrayList<Word> glossary) {
-		this.glossary = glossary;
-	}
-
-	public void addWordGlossary(Word word) {
-		this.glossary.add(word);
-		this.sortByEn();
-	}
-
-	public void addWordStringOnly(String EnglishWord, String FrenchWord) {
-		Word word = new Word(EnglishWord, FrenchWord);
-		glossary.add(word);
-		this.sortByEn();
-	}
-
-	public String search(String keyWord) {
-//ArrayList à faire pour le return ->ajouter les mots ->plusieurs mots même traduction
-		for (Word w : this.glossary) {
-			if (w.getEnglishWord().contains(keyWord)) {
-				return w.toString();
-			}
-		}
-		for (Word w : this.glossary) {
-			if (w.getFrenchWord().contains(keyWord)) {
-				return w.toString();
-			}
-		}
-		return keyWord + " not found";
-	}
+	return keyWord + " not found";
+    }
 
 //	public ArrayList<Word> page(int page) {
 //		ArrayList<Word> list = new ArrayList<Word>();
@@ -76,46 +106,62 @@ public class Glossary {
 //		return list;
 //	}
 
-	public ArrayList<Word> pages(int startPage, int endPage) {
-		ArrayList<Word> list = new ArrayList<Word>();
-		startPage = startPage - 1;
-		endPage = endPage - 1;
-		int startWord = numberOfWordPerPage * startPage;
-		int endWord;
-		if (endPage + 1 == this.numberOfPages()) {
-			endWord = this.glossary.size() - 1;
-		} else {
-			endWord = numberOfWordPerPage * endPage + numberOfWordPerPage - 1;
-		}
-
-		for (int i = startWord; i <= endWord; i++) {
-			list.add(this.glossary.get(i));
-		}
-
-		return list;
-	}
-	
-	public ArrayList<Word> pagesMandatory(int startPage, int endPage) {
-		ArrayList<Word> list = new ArrayList<Word>();
-		startPage = startPage - 1;
-		endPage = endPage - 1;
-		int startWord = numberOfWordPerPage * startPage;
-		int endWord;
-		if (endPage + 1 == this.numberOfPages()) {
-			endWord = this.glossary.size() - 1;
-		} else {
-			endWord = numberOfWordPerPage * endPage + numberOfWordPerPage - 1;
-		}
-
-		for (int i = startWord; i <= endWord; i++) {
-			if (this.glossary.get(i).isMandatory()) {
-				 list.add(this.glossary.get(i));
-			}
-		}
-		return list;
+    /**
+     * Returns all the words of the glossary between <tt>startPage</tt> (included) and <tt>endPage</tt> (included) parameters.
+     * @param startPage : First page to return.
+     * @param endPage : Last page to return.
+     * @return Content of pages between startPage (included) and endPage (included).
+     */
+    public ArrayList<Word> pages(int startPage, int endPage) {
+	ArrayList<Word> list = new ArrayList<Word>();
+	startPage = startPage - 1;
+	endPage = endPage - 1;
+	int startWord = numberOfWordPerPage * startPage;
+	int endWord;
+	if (endPage + 1 == this.numberOfPages()) {
+	    endWord = this.glossary.size() - 1;
+	} else {
+	    endWord = numberOfWordPerPage * endPage + numberOfWordPerPage - 1;
 	}
 
-	public int numberOfPages() {
-		return (int) Math.ceil(glossary.size() / (double) numberOfWordPerPage);
+	for (int i = startWord; i <= endWord; i++) {
+	    list.add(this.glossary.get(i));
 	}
+
+	return list;
+    }
+
+    /**
+     * Returns only the mandatory words of the glossary between <tt>startPage</tt> (included) and <tt>endPage</tt> (included) parameters.
+     * @param startPage : First page to return.
+     * @param endPage : Last page to return.
+     * @return Content of pages between startPage (included) and endPage (included).
+     */
+    public ArrayList<Word> pagesMandatory(int startPage, int endPage) {
+	ArrayList<Word> list = new ArrayList<Word>();
+	startPage = startPage - 1;
+	endPage = endPage - 1;
+	int startWord = numberOfWordPerPage * startPage;
+	int endWord;
+	if (endPage + 1 == this.numberOfPages()) {
+	    endWord = this.glossary.size() - 1;
+	} else {
+	    endWord = numberOfWordPerPage * endPage + numberOfWordPerPage - 1;
+	}
+
+	for (int i = startWord; i <= endWord; i++) {
+	    if (this.glossary.get(i).isMandatory()) {
+		list.add(this.glossary.get(i));
+	    }
+	}
+	return list;
+    }
+    
+    /**
+     * Returns the number of pages of the glossary.
+     * @return The number of pages of the glossary.
+     */
+    public int numberOfPages() {
+	return (int) Math.ceil(glossary.size() / (double) numberOfWordPerPage);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,39 @@
-
+/**
+ * Main class of the program, contains Main function, which is the core of the
+ * app.
+ * 
+ * @author Jean-Camille LAPIERRE, Dorian MOUNIER, Gabriel SAPONARA, Cyril
+ *         PIGEYRE, Medhy DOHOU
+ * @version 1.5
+ */
 
 public class Main {
-	public static Glossary glossary = new Glossary();
-	
-	public static void main(String[] args) {
+    public static Glossary glossary = new Glossary(SerealizationDriver.deSerealizeGlossary());
+
+    /**
+     * Main function of the program, launches every vital function of it.
+     * 
+     * @author Jean-Camille LAPIERRE, Dorian MOUNIER, Gabriel SAPONARA, Cyril
+     *         PIGEYRE, Medhy DOHOU
+     */
+    public static void main(String[] args) {
 //		Glossary glossary = new Glossary();
 //		FilesDrivers.readCsv(glossary);
 //		SerealizationDriver.serealizeGlossary(glossary.getGlossary());
-		
-		glossary.setGlossary(SerealizationDriver.deSerealizeGlossary());
 
-		//glossary.addWordStringOnly("Hello*", "bonjour");
-		
-		 
-		for(Word w: glossary.pages(1,6))
-		{
-		if (w.isMandatory()) {
-		  System.out.println( w.toString());
-		}
-		}
-		System.out.println(glossary.getGlossary().size());
-		System.out.println(glossary.numberOfPages());
-		System.out.println(glossary.search("clé USB"));
-		new ApplicationContext(glossary);
-	
+
+	// glossary.addWordStringOnly("Hello*", "bonjour");
+
+	for (Word w : glossary.pages(1, 6)) {
+	    if (w.isMandatory()) {
+		System.out.println(w.toString());
+	    }
 	}
+	System.out.println(glossary.getGlossary().size());
+	System.out.println(glossary.numberOfPages());
+	System.out.println(glossary.search("clï¿½ USB"));
+	new ApplicationContext(glossary);
+
+    }
 
 }
