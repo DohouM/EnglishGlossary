29932151fe62d32178f275d136965016026ee3a8
jcl <jcl@JCL-I7>
2020-05-16 11:46:46 +0200

    adding PanelProposalTeacher

---
+++ src/PanelProposalTeacher.java
@@ -0,0 +1,19 @@
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class PanelProposalTeacher extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 
+	 */
+	public PanelProposalTeacher() {
+		JLabel addAWord = new JLabel("Add a Word");	
+		this.add(addAWord);
+	}
+
+}
