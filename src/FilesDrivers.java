144487f03d9579e6252cbcf41d24f3afa8960701
jcl <jcl@JCL-I7>
2020-05-17 12:24:55 +0200

    adding write class to FileDriver

--- src/FilesDrivers.java
+++ src/FilesDrivers.java
@@ -1,52 +1,72 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.ArrayList;
 
-public class FilesDrivers implements InterfaceReadWrite{
-	
-	 /**
-	 * 
-	 */
+public class FilesDrivers implements InterfaceReadWrite {
+
+	/**
+	* 
+	*/
 	public FilesDrivers() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public ArrayList<Word> read(String path) { //reading of csv files
-		  BufferedReader reader = null;
-		  ArrayList<Word> list = new ArrayList<Word>();
-		  try {
-		   String line = "";
-		   reader = new BufferedReader(new FileReader(path));  //data.csv
-		   reader.readLine();
-		   
-		   while((line = reader.readLine()) != null) {
-		    String[] fields = line.split(";");
-		    
-		    if(fields.length > 0) {
-				// insert in tab //
-				Word word= new Word(fields[0],fields[1]);
-				list.add(word);	
-		    }
-		   }
-		   
-		   
-		  } catch (Exception ex) {
-		   ex.printStackTrace();
-		  } finally {
-		   try {
-		    reader.close();
-		   } catch (Exception e) {
-		    e.printStackTrace();
-		   }
-		  }
-	return list;
-}
+	public ArrayList<Word> read(String path) { // reading of csv files
+		BufferedReader reader = null;
+		ArrayList<Word> list = new ArrayList<Word>();
+		try {
+			String line = "";
+			reader = new BufferedReader(new FileReader(path)); // data.csv
+			reader.readLine();
+
+			while ((line = reader.readLine()) != null) {
+				String[] fields = line.split(";");
+
+				if (fields.length > 0) {
+					// insert in tab //
+					Word word = new Word(fields[0], fields[1]);
+					list.add(word);
+				}
+			}
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			try {
+				reader.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return list;
+	}
 
 	@Override
-	public void write(ArrayList<Word> list,String path) {
-		// TODO Auto-generated method stub
-		
+	public void write(ArrayList<Word> list, String path) {
+
+		FileWriter fileWriter = null;
+		try {
+			fileWriter = new FileWriter(path);
+
+			fileWriter.append("EN, FR\n");
+			for (Word w : list) {
+				fileWriter.append(w.getEnglishWord());
+				fileWriter.append(",");
+				fileWriter.append(w.getFrenchWord());
+				fileWriter.append("\n");
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			try {
+				fileWriter.flush();
+				fileWriter.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
