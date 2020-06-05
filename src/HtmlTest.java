7bcb643c658c363097f89ea74131962b5e1ac898
jcl <jcl@JCL-I7>
2020-06-05 08:23:43 +0200

    adig a bit of java doc

--- src/HtmlTest.java
+++ src/HtmlTest.java
@@ -1,7 +1,6 @@
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
