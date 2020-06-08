ad507a029308a930982713c6c9a5b51fc9662af0
DOHOU Medhy <medhy.fournier3@gmail.com>
2020-06-08 10:04:52 +0200

    - Removed unused classes and imports. (Thanks UCDetector.)

--- src/AbstractWordWriter.java
+++ src/AbstractWordWriter.java
@@ -1,6 +1,4 @@
 import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
--- src/ObjectOutputStreamWordWriter.java
+++ src/ObjectOutputStreamWordWriter.java
@@ -1,7 +1,6 @@
 import java.io.IOException;
 import java.io.ObjectOutputStream;
-import java.util.ArrayList;
-import java.util.List;
+
 
 
 //TODO javadoc (rewrite, copy/pasted from OISWW)
