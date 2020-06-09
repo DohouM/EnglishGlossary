0edc71b9ab8face19f771cfe75e21ed52398c0e5
DOHOU Medhy <medhy.fournier3@gmail.com>
2020-06-09 09:39:37 +0200

    Changes to ObjectOutputStreamWordWriter : 	+ Added javadoc.

--- src/ObjectOutputStreamWordWriter.java
+++ src/ObjectOutputStreamWordWriter.java
@@ -1,9 +1,6 @@
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 
-
-
-//TODO javadoc (rewrite, copy/pasted from OISWW)
 /**
  * An object that'll write <code>Word</code> objects to ObjectOutputStream
  * <code>in</code>.
@@ -15,22 +12,21 @@ import java.io.ObjectOutputStream;
  */
 public class ObjectOutputStreamWordWriter extends AbstractWordWriter {
     /**
-     * The ObjectInputStream to read Word objects from.
+     * The ObjectInputStream to write <code>Word</code> objects into.
      */
     private final ObjectOutputStream out;
 
     /**
      * The constructor of the class.
      * 
-     * @param theIn the ObjectInputStream to read Word objects from.
+     * @param theOut the ObjectInputStream to write <code>Word</code> objects into.
      */
     public ObjectOutputStreamWordWriter(ObjectOutputStream theOut) {
 	this.out = theOut;
     }
 
     /**
-     * A method to read one Word object from <code>in</code>.
-     * @return The word read by the <code>in</code> attribute.
+     * A method to write one Word object from <code>in</code>.
      * @throws IOException The stream is closed/doesn't read a Word object.
      */
     public void writeWord(Word theWord) throws IOException {
