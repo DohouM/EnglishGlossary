214e3937aa493037736d8e1c65dead7e63edd536
sebastienjean <sebastien.jean@univ-grenoble-alpes.fr>
2020-09-21 11:27:13 +0200

    Removed teacher mode support
    - this mode is not required for now, it will be rebuilt when needed

--- src/gui/EnglishGlossaryGUI.java
+++ src/gui/EnglishGlossaryGUI.java
@@ -14,13 +14,11 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 import core.Glossary;
-import core.Password_Check;
 import io.CsvFileSerializationDriver;
 
 /**
@@ -38,10 +36,6 @@ public class EnglishGlossaryGUI extends JFrame implements ActionListener {
 	public static Glossary glossary; // store the when the GUI is lunched
 
 	/**
-	 * A JButton object used to connect as a teacher, after entering the password.
-	 */
-	private JButton connect = new JButton("Connect as teacher");
-	/**
 	 * 
 	 */
 	private JButton enterGlossary = new JButton("Enter Glossary");
@@ -54,18 +48,11 @@ public class EnglishGlossaryGUI extends JFrame implements ActionListener {
 	 * A JTextField used for the user to input it's username.
 	 */
 	private JTextField id = new JTextField("Your User Name");
+
 	/**
 	 * The currently logged user.
 	 */
 	private String user;
-	/**
-	 * The JPasswordField used for the input for teacher password.
-	 */
-	private JPasswordField password = new JPasswordField("");
-
-	/**
-	 * The MD5 hash of the current teacher password.
-	 */
 
 	/**
 	 * The constructor of the EnglishGlossaryGUI class. Create a JFrame and set it
@@ -130,63 +117,14 @@ public class EnglishGlossaryGUI extends JFrame implements ActionListener {
 	}
 
 	public void actionPerformed(ActionEvent arg0) { // Method that will be called when clicking on the button
-		if (arg0.getSource() == this.enterGlossary) {
-			if (this.id.getText().equals("metgec")) {
-				this.user = new String(this.id.getText());
-				this.title.setText("E-GLOSSARY AS " + this.user);
-				this.setSize(500, 150);
-				this.setLocationRelativeTo(null);
 
-				JPanel conectpan = new JPanel();
-				conectpan.setBackground(Color.white);
-				Font police = new Font("impact", Font.ITALIC, 30);
-				// It is applied to the JLabel
-				this.title.setFont(police);
-				// Changing the text color
-				this.title.setForeground(Color.red);
-				// The text alignment is modified using static attributes
-				// JLabel class
-				this.title.setHorizontalAlignment(SwingConstants.CENTER);
-
-				this.id.setHorizontalAlignment(SwingConstants.CENTER);
-				this.password.setHorizontalAlignment(SwingConstants.CENTER);
-
-				conectpan.setLayout(new BorderLayout());
-				conectpan.add(this.title, BorderLayout.NORTH);
-
-				conectpan.add(this.password, BorderLayout.CENTER);
-				this.connect.addActionListener(this);
-				conectpan.add(this.connect, BorderLayout.SOUTH);
-				this.setContentPane(conectpan);
-			} else {
-				this.user = new String(this.id.getText());
-				title.setText("E-GLOSSARY AS " + this.user);
-				this.setSize(1000, 800);
-				this.setLocationRelativeTo(null);
-				JTabbedPane pane = loadStudentPanel();
-				// frame
-				this.setContentPane(pane);
-			}
-			System.out.println("Glossary lunched");
-		}
-
-		if (arg0.getSource() == this.connect) {
-			;
-			char[] pass = this.password.getPassword();
-			String passString = new String(pass);
-			if (Password_Check.comparable(passString)) {
-				System.out.println("Good PassWord");
-				this.setSize(1000, 800);
-				this.setLocationRelativeTo(null);
-				JTabbedPane pane = loadTeacherPanel();
-
-				// frame
-				this.setContentPane(pane);
-			} else {
-				JOptionPane.showMessageDialog(null, "Password is incorrect, try again.");
-			}
-
-		}
+		this.user = this.id.getText();
+		this.title.setText("E-GLOSSARY AS " + this.user);
+		this.setSize(1000, 800);
+		this.setLocationRelativeTo(null);
+		JTabbedPane pane = loadStudentPanel();
+		// frame
+		this.setContentPane(pane);
 	}
 
 	/**
@@ -212,37 +150,4 @@ public class EnglishGlossaryGUI extends JFrame implements ActionListener {
 		pane.add(titre3, new PanelProposal());
 		return pane;
 	}
-
-	/**
-	 * Load the teacher window.
-	 * 
-	 * @return The student JTabbedPane object, which contains the tabs accessible to
-	 *         teacher.
-	 */
-	private JTabbedPane loadTeacherPanel() {
-		// Pan
-		JTabbedPane pane = new JTabbedPane();
-		String titre = "Glossary";
-		pane.add(titre, new PanelGlossary());
-		pane.setTabComponentAt(0, new Tabs(titre, pane));
-		// Pan
-
-		String titre2 = "Quiz";
-		pane.add(titre2, new PanelQuiz());
-
-		// Pan
-		String titre3 = "Proposal";
-		pane.add(titre3, new PanelProposalTeacher());
-
-		// Pan test generator
-		String titre4 = "Test Generator";
-		pane.add(titre4, new PanelTestGenerator());
-
-		// Pan Modify
-		String titre5 = "Modify Glossary";
-		pane.add(titre5, new PanelModifyGlossary());
-
-		return pane;
-	}
-
 }
